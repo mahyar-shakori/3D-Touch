01ee6e239fc4544f7e84b89deb5804e3cda29701
Mahyar Shakouri <shakourimahyar@outlook.com>
2023-06-08 08:26:29 +0330

    Initial Commit

---
+++ as/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  as
+//
+//  Created by Mahyar on 6/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
